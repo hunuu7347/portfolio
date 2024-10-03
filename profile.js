e1443ae0d3a9fdf0db74e875488f5ed03025c9af
Hue Nguyen <hue7347@gmail.com>
2024-10-04 02:53:07 +1300

    add portfolio

---
+++ profile.js
@@ -0,0 +1,15 @@
+export function profile() {
+return 
+<>
+<h2>Profile</h2>
+        <p>
+          I started in Tech fairly recently but have loved every step of the
+          journey. It is the best thing to see the final product and know that
+          all the work I have put through it has shown. Below is a collection of
+          my projects starting from recently to when I first started at Dev
+          Academy. With the first on the list is a game I worked on with my
+          Group Calli2Welli from PowerUp Game Jam just in the last weekend!
+          
+        </p>
+        </>
+}
